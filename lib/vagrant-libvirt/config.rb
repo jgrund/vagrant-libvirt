55c56a8f0071e64c5ffba6ab346af656e312cef3
Joe Grund <jgrund@whamcloud.io>
2018-07-18 08:35:00 -0400

    Update Virtio scsi patch to latest

--- lib/vagrant-libvirt/config.rb
+++ lib/vagrant-libvirt/config.rb
@@ -84,6 +84,7 @@ module VagrantPlugins
       attr_accessor :machine_virtual_size
       attr_accessor :disk_bus
       attr_accessor :disk_device
+      attr_accessor :disk_controller_model
       attr_accessor :nic_model_type
       attr_accessor :nested
       attr_accessor :volume_cache
@@ -204,6 +205,7 @@ module VagrantPlugins
         @machine_virtual_size = UNSET_VALUE
         @disk_bus          = UNSET_VALUE
         @disk_device       = UNSET_VALUE
+        @disk_controller_model = UNSET_VALUE
         @nic_model_type    = UNSET_VALUE
         @nested            = UNSET_VALUE
         @volume_cache      = UNSET_VALUE
@@ -682,6 +684,7 @@ module VagrantPlugins
         @machine_virtual_size = nil if @machine_virtual_size == UNSET_VALUE
         @disk_bus = 'virtio' if @disk_bus == UNSET_VALUE
         @disk_device = 'vda' if @disk_device == UNSET_VALUE
+        @disk_controller_model = 'auto' if @disk_controller_model == UNSET_VALUE
         @nic_model_type = nil if @nic_model_type == UNSET_VALUE
         @nested = false if @nested == UNSET_VALUE
         @volume_cache = 'default' if @volume_cache == UNSET_VALUE
